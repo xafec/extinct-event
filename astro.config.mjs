b66bab44a22408e9b7a95cd0fbe1531e774e1216
xafec <wivvz@yandex.ru>
2024-05-05 23:48:50 +0300

    del auth for cloudflare build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import auth from "auth-astro";
 import react from "@astrojs/react";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), auth(), react()],
+  integrations: [tailwind(), react()],
   output: "server",
   adapter: cloudflare({
     imageService: "cloudflare",
