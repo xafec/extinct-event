2e6db0ab11d162562333ba97aa99cf79b99afaf2
xafec <wivvz@yandex.ru>
2024-05-05 23:35:18 +0300

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import auth from "auth-astro";
 import react from "@astrojs/react";
-
-import cloudflare from "@astrojs/cloudflare";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), auth(), react()],
   output: "server",
-  adapter: cloudflare({
-    imageService: "cloudflare",
+  adapter: node({
+    mode: "standalone",
   }),
 });
