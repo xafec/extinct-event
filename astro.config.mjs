7b5f2ff5fd05d360b5e369190e5cf46435542023
xafec <wivvz@yandex.ru>
2024-05-05 23:58:36 +0300

    nahui cloudflare, will test vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import cloudflare from "@astrojs/cloudflare";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: cloudflare({
-    imageService: "passthrough",
+  adapter: vercel({
+    imageService: true,
   }),
 });
