4e6eae21079e855e34fae5f49bed1fb1782b8c36
xafec <wivvz@yandex.ru>
2024-05-05 23:18:36 +0300

    Simple start

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
 import auth from "auth-astro";
-
 import react from "@astrojs/react";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), auth(), react()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: cloudflare({
+    imageService: "cloudflare",
+  }),
 });
